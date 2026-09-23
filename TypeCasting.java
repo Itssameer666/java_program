1447682888370cf61ad028727f3060297a935e58
Itssameer666 <sa8506818sameerali@gmail.com>
2026-09-23 23:20:26 +0530

    TypeCasting

---
+++ TypeCasting.java
@@ -0,0 +1,10 @@
+public class TypeCasting {      //typecasting
+    public static void main(String[] args) {
+        double num = 45.78;
+        int value = (int) num;   // Explicit type casting
+
+        System.out.println("Double value: " + num);
+        System.out.println("Integer value: " + value);
+    }
+}
+
